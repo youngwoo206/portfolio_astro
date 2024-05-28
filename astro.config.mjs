8ac411295e8f637f7e2c0e846b27cfdaf9624e65
Youngwoo <youngwoo206@gmail.com>
2024-05-28 10:28:38 -0400

    adding tailwind support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   output: "server",
   adapter: vercel(),
 });
+
+// import { defineConfig } from 'astro/config';
+// import tailwind from '@astrojs/tailwind';
+
+// export default defineConfig({
+//   // ...
+//   integrations: [tailwind()],
+// });
