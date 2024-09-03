84660cea58d48da6a5bc2c89b6a94d89c41f0e5b
Youngwoo <youngwoo206@gmail.com>
2024-09-02 21:23:29 -0400

    basic commands

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -10,6 +10,13 @@ import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export default function Landing() {
   const [isMobile, setIsMobile] = useState(window.innerWidth > 600);
+  const [terminalLineData, setTerminalLineData] = useState([
+    <TerminalOutput>Welcome to my portfolio website!</TerminalOutput>,
+    <TerminalOutput>
+      <span style={{ color: "#A995F9" }}>Type "help" for all commands</span>
+    </TerminalOutput>,
+  ]);
+  const [prevCommands, setPrevCommands] = useState<string[]>([]);
 
   const updateMedia = () => {
     setIsMobile(window.innerWidth > 720);
@@ -20,27 +27,115 @@ export default function Landing() {
     return () => window.removeEventListener("resize", updateMedia);
   });
 
-  const [terminalLineData, setTerminalLineData] = useState([
-    <TerminalOutput>Welcome to my portfolio website!</TerminalOutput>,
-  ]);
-
   const handleInput = (terminalInput: string) => {
+    const prevLines = handlePrevCommands(terminalInput);
+    switch (terminalInput) {
+      case "help":
+        handleHelp();
+        break;
+      case "clear":
+        handleClear();
+        break;
+      case "exp":
+        handleExperience();
+        break;
+      case "skills":
+        handleSkills();
+        break;
+      default:
+        setTerminalLineData([
+          ...prevLines,
+          <TerminalOutput>{`$ ${terminalInput}`}</TerminalOutput>,
+        ]);
+    }
+  };
+
+  const handlePrevCommands = (terminalInput: string) => {
+    prevCommands
+      ? setPrevCommands((prev) => [...prev, terminalInput])
+      : setPrevCommands(["Welcome to my portfolio website!"]);
+    const prevLines = prevCommands.map((cmd) => (
+      <TerminalOutput>{`$ ${cmd}`}</TerminalOutput>
+    ));
+    return prevLines;
+  };
+
+  const handleClear = () => {
+    setPrevCommands([]);
     setTerminalLineData([
-      <TerminalOutput>{`command: ${terminalInput}`}</TerminalOutput>,
+      <TerminalOutput>Welcome to my portfolio website!</TerminalOutput>,
+      <TerminalOutput>
+        <span style={{ color: "#A995F9" }}>Type "help" for all commands</span>
+      </TerminalOutput>,
     ]);
+  };
+
+  const handleHelp = () => {
+    const cmds = [
+      "help -> lists out all commands",
+      "clear -> clears the terminal",
+      "skills -> my skills",
+      "exp -> my work experience",
+    ];
+    const terminalLines = cmds.map((cmd) => (
+      <TerminalOutput>{`${cmd}`}</TerminalOutput>
+    ));
+    setTerminalLineData(terminalLines);
+    console.log(terminalLineData);
+  };
+
+  const handleExperience = () => {
+    const exp = [
+      "Ada: SWE intern (Jun 2024-Aug 2024)",
+      "Junior_AI: SWE intern (Aug 2023-Dec 2023)",
+      "Baselane: QA Developer (May 2023-Aug 2023)",
+      "Saige: Full Stack Developer (Feb 2023-May 2023)",
+    ];
+    const expLines = exp.map((ex) => (
+      <TerminalOutput>{`${ex}`}</TerminalOutput>
+    ));
+    setTerminalLineData(expLines);
+  };
+
+  const handleSkills = () => {
+    const skills = [
+      {
+        heading: "Languages",
+        content: "TypeScript, Python, Java, HTML & CSS, SQL, Bash, C#",
+      },
+      {
+        heading: "Frameworks",
+        content:
+          "React, Next.js, Flask, Astro.js, Node.js, React Native, Spring Boot",
+      },
+      {
+        heading: "Technologies",
+        content: "AWS, MySQL, PostgreSQL, GCP, Redis, Github Actions",
+      },
+    ];
+
+    const skillLines = skills.map((skill) => (
+      <TerminalOutput>
+        <div className="wrap">
+          <span className="terminal-heading">{`${skill.heading}: `}</span>
+          {skill.content}
+        </div>
+      </TerminalOutput>
+    ));
+    setTerminalLineData(skillLines);
   };
 
   return (
     <div className="landing">
       <div className="terminal-container">
-        <p className="heading">Hi, I'm Youngwoo</p>
+        <p className="heading">Hi, I'm Youngwoo 👋</p>
         <Terminal
           name=""
           colorMode={ColorMode.Dark}
           height="300px"
           onInput={(terminalInput) => handleInput(terminalInput)}
         >
-          {terminalLineData}
+          <div className="terminal-area">{terminalLineData}</div>
         </Terminal>
       </div>
       <div className="media-links-div">
